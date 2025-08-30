8622b2317569c929366f13e0702691c848ba4c50
Arnav Deore <156898120+arnav200525@users.noreply.github.com>
2025-08-31 01:10:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 import google.generativeai as genai
 import pdfplumber
 
-
 def extraction(pdf_name):
     with pdfplumber.open(pdf_name) as p:
         for page in p.pages:
